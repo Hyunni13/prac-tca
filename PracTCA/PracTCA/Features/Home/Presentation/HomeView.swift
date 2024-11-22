14a61ecadd6308ea038092b17f02762039ee6c55
Sanghyun <dev.sanghyun@gmail.com>
2024-11-22 18:46:27 +0900

    [UI] HomeView 수정
    - 채팅앱과 같이 유저 목록 표출

--- PracTCA/PracTCA/Features/Home/Presentation/HomeView.swift
+++ PracTCA/PracTCA/Features/Home/Presentation/HomeView.swift
@@ -11,7 +11,32 @@ struct HomeView: View {
     @ObservedObject var viewModel: HomeViewModel
     
     var body: some View {
-        Text(viewModel.store.state.users.debugDescription)
+        VStack {
+            HStack {
+                Text("Friends")
+                    .font(.system(size: 24))
+                Spacer()
+            }
+            Spacer()
+                .frame(height: 40)
+            
+            ScrollView {
+                VStack(spacing: 20) {
+                    ForEach(viewModel.store.state.users) { user in
+                        HStack(alignment: .center, spacing: 10) {
+                            Image(systemName: "person.crop.circle.fill")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 28, height: 28)
+                            Text(user.name)
+                                .font(.system(size: 16))
+                            Spacer()
+                        }
+                    }
+                }
+            }
+        }
+        .padding()
     }
 }
 
